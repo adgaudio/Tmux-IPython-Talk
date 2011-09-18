bf3c8976b6745fbd58616cb0ce53395a58fefd94
Alex Gaudio <adgaudio@gmail.com>
2011-09-18 01:17:11 -0400

    plotting example tweak

--- ipython/plotting_data_example.py
+++ ipython/plotting_data_example.py
@@ -1,2 +1,3 @@
 a = get_ipython().getoutput(u"echo show table status | mysql -uroot drupal")
-plot([int(y) for y in x.split()] for x in a.fields(4,5).l)
+plot([int(y) for x in a.fields(4,5).l[1:] for y in x.split()])
+
